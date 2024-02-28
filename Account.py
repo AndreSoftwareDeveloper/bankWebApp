dbf070f99894538202c06c1de98d0a0ecd658d2a
Andre <andriej2301@gmail.com>
2024-02-28 17:31:23 +0100

    feature: Added template

--- Account.py
+++ Account.py
@@ -1,12 +1,16 @@
 class Account:
     _instance = None
 
-    def __new__(cls, balance, account_number):
+    def __new__(cls, balance: int, account_number: int, history, owner):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
+
             cls._instance.balance = balance
             cls._instance.account_number = account_number
+            cls._instance.history = history
+            cls._instance.owner = owner
+
         return cls._instance
 
-    def __init__(self, balance, account_number):
+    def __init__(self, balance, account_number, history, owner):
         pass
